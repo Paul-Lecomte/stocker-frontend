396997b11c3520265960993d206aaa4f9dc52314
Paul Lecomte <164857566+Paul-Lecomte@users.noreply.github.com>
2025-01-30 08:26:20 +0100

    added a footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Typography } from "@material-tailwind/react";
 
 const Footer = () => {
     return (
-        <div>
-            
-        </div>
+        <footer className="w-full py-4 text-gray-400 z-50"
+                style={{ marginLeft: "25%", width: "75%", position: "relative", background: "#101923" }}>
+            <Typography variant="small" className="text-center text-gray-400">
+                © {new Date().getFullYear()} Stocker. All rights reserved.
+            </Typography>
+        </footer>
     );
 };
 
