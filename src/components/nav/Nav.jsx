613e61a35a87535c45f7667eed3506aec957de30
Paul Lecomte <164857566+Paul-Lecomte@users.noreply.github.com>
2025-02-07 09:01:50 +0100

    changed the auth check to the backend for more security

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import logo from '../../assets/stocker_name.svg';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import logo from "../../assets/stocker_name.svg";
 import {
     Card,
     List,
@@ -16,15 +17,35 @@ import { MdOutlineInventory } from "react-icons/md";
 import { Link, useLocation } from "react-router-dom";
 
 const Nav = () => {
-    // Get userInfo from localStorage and parse it
-    const userInfoFromStorage = localStorage.getItem('userInfo');
+    const [userRole, setUserRole] = useState(null);
+
+    // Get userInfo from localStorage (excluding role)
+    const userInfoFromStorage = localStorage.getItem("userInfo");
     const userInfo = userInfoFromStorage ? JSON.parse(userInfoFromStorage).user : null;
 
-    // Check for the user roles
-    const isSuperAdmin = userInfo && userInfo.role === 'superadmin';
-    const isAdmin = userInfo && userInfo.role === 'admin';
+    useEffect(() => {
+        const fetchUserRole = async () => {
+            if (userInfo?._id) {
+                try {
+                    const response = await axios.get(
+                        `http://localhost:3000/api/user/role/${userInfo._id}`,
+                        { withCredentials: true }
+                        );
+                    setUserRole(response.data.role);
+                } catch (error) {
+                    console.error("Error fetching user role:", error);
+                }
+            }
+        };
 
-    // Get the current URL
+        fetchUserRole();
+    }, [userInfo?._id]);
+
+    // Role checks
+    const isSuperAdmin = userRole === "superadmin";
+    const isAdmin = userRole === "admin";
+
+    // Get current URL
     const location = useLocation();
 
     return (
@@ -38,25 +59,19 @@ const Nav = () => {
                 />
             </div>
             <List className="h-2/3 flex flex-col justify-around">
-                <ListItem
-                    className={`text-2xl hover:bg-gray-200 transition-colors duration-200 ${location.pathname === "/" ? "text-blue-500" : "text-white"}`}
-                >
+                <ListItem className={`text-2xl hover:bg-gray-200 transition-colors duration-200 ${location.pathname === "/" ? "text-blue-500" : "text-white"}`}>
                     <ListItemPrefix>
                         <PresentationChartBarIcon className="h-7 w-7"/>
                     </ListItemPrefix>
                     <Link to={"/"}>Dashboard</Link>
                 </ListItem>
-                <ListItem
-                    className={`text-2xl hover:bg-gray-200 transition-colors duration-200 ${location.pathname === "/movement" ? "text-blue-500" : "text-white"}`}
-                >
+                <ListItem className={`text-2xl hover:bg-gray-200 transition-colors duration-200 ${location.pathname === "/movement" ? "text-blue-500" : "text-white"}`}>
                     <ListItemPrefix>
                         <InboxIcon className="h-7 w-7"/>
                     </ListItemPrefix>
                     <Link to={"/movement"}>Movement</Link>
                 </ListItem>
-                <ListItem
-                    className={`text-2xl hover:bg-gray-200 transition-colors duration-200 ${location.pathname === "/products" ? "text-blue-500" : "text-white"}`}
-                >
+                <ListItem className={`text-2xl hover:bg-gray-200 transition-colors duration-200 ${location.pathname === "/products" ? "text-blue-500" : "text-white"}`}>
                     <ListItemPrefix>
                         <ShoppingBagIcon className="h-7 w-7"/>
                     </ListItemPrefix>
@@ -66,19 +81,13 @@ const Nav = () => {
                 {/* Conditionally render Inventory link based on admin or superadmin roles */}
                 {(isSuperAdmin || isAdmin) && (
                     <>
-                        <ListItem
-                            className={`text-2xl hover:bg-gray-200 transition-colors duration-200 ${location.pathname === "/inventory" ? "text-blue-500" : "text-white"}`}
-                        >
+                        <ListItem className={`text-2xl hover:bg-gray-200 transition-colors duration-200 ${location.pathname === "/inventory" ? "text-blue-500" : "text-white"}`}>
                             <ListItemPrefix>
                                 <MdOutlineInventory className="h-7 w-7"/>
                             </ListItemPrefix>
                             <Link to={"/inventory"}>Inventory</Link>
                         </ListItem>
-
-                        {/* New Stock Movement History link for admin only */}
-                        <ListItem
-                            className={`text-2xl hover:bg-gray-200 transition-colors duration-200 ${location.pathname === "/stock_movement_history" ? "text-blue-500" : "text-white"}`}
-                        >
+                        <ListItem className={`text-2xl hover:bg-gray-200 transition-colors duration-200 ${location.pathname === "/stock_movement_history" ? "text-blue-500" : "text-white"}`}>
                             <ListItemPrefix>
                                 <ShoppingBagIcon className="h-7 w-7"/>
                             </ListItemPrefix>
@@ -88,20 +97,15 @@ const Nav = () => {
                 )}
                 {/* Conditionally render User Management link based on the user's role */}
                 {isSuperAdmin && (
-                    <ListItem
-                        className={`text-2xl hover:bg-gray-200 transition-colors duration-200 ${location.pathname === "/user-management" ? "text-blue-500" : "text-white"}`}
-                    >
+                    <ListItem className={`text-2xl hover:bg-gray-200 transition-colors duration-200 ${location.pathname === "/user-management" ? "text-blue-500" : "text-white"}`}>
                         <ListItemPrefix>
                             <UserCircleIcon className="h-7 w-7"/>
                         </ListItemPrefix>
                         <Link to={"/user-management"}>User Management</Link>
                     </ListItem>
                 )}
-
                 {/* Notifications link */}
-                <ListItem
-                    className={`text-2xl hover:bg-gray-200 transition-colors duration-200 ${location.pathname === "/notifications" ? "text-blue-500" : "text-white"}`}
-                >
+                <ListItem className={`text-2xl hover:bg-gray-200 transition-colors duration-200 ${location.pathname === "/notifications" ? "text-blue-500" : "text-white"}`}>
                     <ListItemPrefix>
                         <InboxIcon className="h-7 w-7" />
                     </ListItemPrefix>
